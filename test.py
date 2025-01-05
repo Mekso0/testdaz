f492038c9cbd5f99d1dba7b4bcd352786703ea93
Mekso0 <160159534+Mekso0@users.noreply.github.com>
2025-01-05 11:46:53 +0500

    Add files via upload

--- test.py
+++ test.py
@@ -1,22 +1,20 @@
 import telebot
-import os
-from dotenv import load_dotenv
+from telebot import types
 
-load_dotenv()
+# Замените 'YOUR_BOT_TOKEN' на токен вашего бота
+TOKEN = '7272558954:AAHxz7PhTsRPthRqCZPanvnuu1Roig6B4Lk'
 
-TOKEN = os.getenv("7272558954:AAH77qVl2XkjmUOEFTF6vq9VPBw2bu0dOEA")  # Используйте os.getenv() для получения токена из переменных окружения
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
-    gif_path = "seal.gif"  # Путь к вашей гифке. Поместите файл seal.gif в ту же папку, где находится скрипт
+def start_message(message):
+    chat_id = message.chat.id
     
-    if not os.path.exists(gif_path):
-       bot.send_message(message.chat.id, "Ошибка: гифка не найдена. Пожалуйста, убедитесь, что файл seal.gif находится в той же папке, что и скрипт.")
-       return
-    
-    with open(gif_path, 'rb') as gif:
-      bot.send_animation(message.chat.id, gif, caption="🦭 Привет! Это тест бот здесь ничего интересного)")
+    # Отправляем гифку
+    with open('seal.gif', 'rb') as gif_file:  # Замените 'seal.gif' на путь к вашей гифке
+        bot.send_animation(chat_id, gif_file, caption='🦭 Привет! Это тест бот здесь ничего интересного)')
 
+
+# Запускаем бота
 if __name__ == '__main__':
     bot.polling(none_stop=True)
